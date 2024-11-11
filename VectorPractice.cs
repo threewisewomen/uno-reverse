67dbac898b29e04fb9f0b69072406e5799d8f325
John Mathew <jmathew@acilogistix.com>
2024-11-11 11:39:22 +0530

    vectors

--- VectorPractice.cs
+++ VectorPractice.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace uno_reverse
 {
-    internal class VectorPractice
+    public class VectorPractice
     {
+        public static double Find3dvectorDotProduct(int ax, int ay, int az,
+                                                    int bx, int by, int bz)
+        {
+
+            double dotProduct = (ax * bx) + (ay * by) + (az * bz);
+            return dotProduct;
+
+        }
+        public static (List<double>,List<double>) find3dvectorProjection(int ax, int ay, int az,
+                                                    int bx, int by, int bz)
+        {
+            //let u = ax + ay + az and v = bx + by + bz
+            double dotProduct = (ax * bx) + (ay * by) + (az * bz);
+            double magnitudeU = Math.Sqrt((ax*ax) + (ay*ay) + (az*az));
+            double magnitudeV = Math.Sqrt((bx*bx) + (by*by) + (bz*bz));
+
+            List<double> projUuntoV = new List<double>
+            {
+                (dotProduct/ (magnitudeU * magnitudeU)) * ax,
+                (dotProduct/ (magnitudeU * magnitudeU)) * ay,
+                (dotProduct/ (magnitudeU * magnitudeU)) * az
+            };
+
+            List<double> projVuntoU = new List<double>
+            {
+                (dotProduct/ (magnitudeV * magnitudeV)) * bx,
+                (dotProduct/ (magnitudeV * magnitudeV)) * by,
+                (dotProduct/ (magnitudeV * magnitudeV)) * bz
+            };
+            return (projUuntoV, projVuntoU);
+
+
+
+        }
     }
 }
